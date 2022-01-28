68842242cdd7741b9aebe3f95c44cb06c08dbf8d
Andrew Laychak <alaychak@harriscomputer.com>
2022-01-28 09:31:54 -0500

    refactor: :recycle: update alias'

--- packages/directives/src/index.ts
+++ packages/directives/src/index.ts
@@ -5,7 +5,7 @@
     Email: ALaychak@harriscomputer.com
 
     Created At: 01-24-2022 03:11:36 PM
-    Last Modified: 01-24-2022 03:11:55 PM
+    Last Modified: 01-28-2022 09:28:37 AM
     Last Updated By: Andrew Laychak
 
     Description: Index file for the directives package
@@ -17,7 +17,8 @@
 // #endregion
 
 // #region Imports
-import { DateDirective, NumberDirective } from '@directives/All';
+import DateDirective from '@directives/Date';
+import NumberDirective from '@directives/Number';
 // #endregion
 
 // #region Exports
